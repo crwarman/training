554aa0209f33a428bdfba12d781c5ada950881bb
Craig Warman <crwarman@users.noreply.github.com>
2017-05-05 11:54:37 -0400

    Update install_jdbc_drivers.sh
    Updated SDC from 2.2.0.0 to 2.5.0.0
    Updated mysql connector from 5.1.39 to 5.1.42
    Updated postgres from 9.4.1212 to 42.0.0

--- install_jdbc_drivers.sh
+++ install_jdbc_drivers.sh
@@ -1,6 +1,6 @@
 # install JDBC drivers into SDC
-cd /root/training/streamsets-datacollector-2.2.0.0
+cd /root/training/streamsets-datacollector-2.5.0.0
 mkdir -p libs-common-lib/streamsets-datacollector-jdbc-lib/lib
-cp ../mysql-connector-java-5.1.39-bin.jar libs-common-lib/streamsets-datacollector-jdbc-lib/lib
-cp ../postgresql-9.4.1212.jar libs-common-lib/streamsets-datacollector-jdbc-lib/lib
+cp ../mysql-connector-java-5.1.42-bin.jar libs-common-lib/streamsets-datacollector-jdbc-lib/lib
+cp ../postgresql-42.0.0.jar libs-common-lib/streamsets-datacollector-jdbc-lib/lib
 echo "export STREAMSETS_LIBRARIES_EXTRA_DIR=libs-common-lib" >> libexec/sdc-env.sh
