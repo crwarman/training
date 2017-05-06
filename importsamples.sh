9ace4d6993a5f9ece7807c81191e925487731f99
Craig Warman <crwarman@users.noreply.github.com>
2017-05-06 07:07:58 -0400

    Updated to SDC 2.5.0.0

--- importsamples.sh
+++ importsamples.sh
@@ -1,12 +1,12 @@
-streamsets-datacollector-2.2.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_1_1" -f samples/sample_pipelines/Lesson_1_1.json
-streamsets-datacollector-2.2.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_1_2" -f samples/sample_pipelines/Lesson_1_2.json
-streamsets-datacollector-2.2.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_2_1" -f samples/sample_pipelines/Lesson_2_1.json
-streamsets-datacollector-2.2.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_2_2" -f samples/sample_pipelines/Lesson_2_2.json
-streamsets-datacollector-2.2.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_1" -f samples/sample_pipelines/Lesson_3_1.json
-streamsets-datacollector-2.2.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_2" -f samples/sample_pipelines/Lesson_3_2.json
-streamsets-datacollector-2.2.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4_1" -f samples/sample_pipelines/Lesson_4_1.json
-streamsets-datacollector-2.2.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4_2" -f samples/sample_pipelines/Lesson_4_2.json
-streamsets-datacollector-2.2.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "Language Processors" -f samples/sample_pipelines/LanguageProcessors.json
-streamsets-datacollector-2.2.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "Lookup Processors" -f samples/sample_pipelines/LookupProcessors.json
-streamsets-datacollector-2.2.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "Spark Sample" -f samples/sample_pipelines/SparkTaxiTransactions.json
-streamsets-datacollector-2.2.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4_1_postgres" -f samples/sample_pipelines/Lesson_4_1_postgres.json
+streamsets-datacollector-2.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_1_1" -f samples/sample_pipelines/Lesson_1_1.json
+streamsets-datacollector-2.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_1_2" -f samples/sample_pipelines/Lesson_1_2.json
+streamsets-datacollector-2.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_2_1" -f samples/sample_pipelines/Lesson_2_1.json
+streamsets-datacollector-2.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_2_2" -f samples/sample_pipelines/Lesson_2_2.json
+streamsets-datacollector-2.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_1" -f samples/sample_pipelines/Lesson_3_1.json
+streamsets-datacollector-2.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_2" -f samples/sample_pipelines/Lesson_3_2.json
+streamsets-datacollector-2.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4_1" -f samples/sample_pipelines/Lesson_4_1.json
+streamsets-datacollector-2.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4_1_postgres" -f samples/sample_pipelines/Lesson_4_1_postgres.json
+streamsets-datacollector-2.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4_2" -f samples/sample_pipelines/Lesson_4_2.json
+streamsets-datacollector-2.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "Language Processors" -f samples/sample_pipelines/LanguageProcessors.json
+streamsets-datacollector-2.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "Lookup Processors" -f samples/sample_pipelines/LookupProcessors.json
+streamsets-datacollector-2.5.0.0/bin/streamsets cli -U http://localhost:18630 store import -n "Spark Sample" -f samples/sample_pipelines/SparkTaxiTransactions.json
