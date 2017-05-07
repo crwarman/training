d22da02578df837c043eb89b7e415e15dc5bb892
Craig Warman <crwarman@users.noreply.github.com>
2017-05-07 13:08:37 -0400

    Added retail_db-start.sql

--- setup_rdbms.sh
+++ setup_rdbms.sh
@@ -1,6 +1,8 @@
 #Setup a MySQL Database
 mysql -uroot -ppassword -e 'CREATE DATABASE streamsetsdemo;'
 python init_rdbms_data.py mysql
+mysql -uroot -ppassword -e 'CREATE DATABASE training;'
+mysql -uroot -ppassword -e < /root/training/samples/data/retail_db-start.sql
 
 #setup a postgres DATABASE
 sudo -u postgres createdb streamsetsdemo
