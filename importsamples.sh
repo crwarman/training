b387024a1174fc8681302d62f5ea3ad9cdc4dcf0
Craig Warman <crwarman@users.noreply.github.com>
2017-05-08 11:14:45 -0400

    Removed additional pipelines
    Removed these lines:
    $SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4_1" -f $SAMPLE_PIPELINES/Lesson_4_1.json
    $SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4_1_postgres" -f $SAMPLE_PIPELINES/Lesson_4_1_postgres.json
    $SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4_2" -f $SAMPLE_PIPELINES/Lesson_4_2.json
    $SDC/streamsets cli -U http://localhost:18630 store import -n "Language Processors" -f $SAMPLE_PIPELINES/LanguageProcessors.json
    $SDC/streamsets cli -U http://localhost:18630 store import -n "Lookup Processors" -f $SAMPLE_PIPELINES/LookupProcessors.json
    $SDC/streamsets cli -U http://localhost:18630 store import -n "Spark Sample" -f $SAMPLE_PIPELINES/SparkTaxiTransactions.json

--- importsamples.sh
+++ importsamples.sh
@@ -6,9 +6,3 @@ $SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_2_1" 
 $SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_2_2" -f $SAMPLE_PIPELINES/Lesson_2_2.json
 $SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_1" -f $SAMPLE_PIPELINES/Lesson_3_1.json
 $SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_2" -f $SAMPLE_PIPELINES/Lesson_3_2.json
-$SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4_1" -f $SAMPLE_PIPELINES/Lesson_4_1.json
-$SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4_1_postgres" -f $SAMPLE_PIPELINES/Lesson_4_1_postgres.json
-$SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_4_2" -f $SAMPLE_PIPELINES/Lesson_4_2.json
-$SDC/streamsets cli -U http://localhost:18630 store import -n "Language Processors" -f $SAMPLE_PIPELINES/LanguageProcessors.json
-$SDC/streamsets cli -U http://localhost:18630 store import -n "Lookup Processors" -f $SAMPLE_PIPELINES/LookupProcessors.json
-$SDC/streamsets cli -U http://localhost:18630 store import -n "Spark Sample" -f $SAMPLE_PIPELINES/SparkTaxiTransactions.json
