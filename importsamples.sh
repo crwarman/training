10c4a1e6294e355bfad0b6263be61f87d35b58ae
Craig Warman <crwarman@users.noreply.github.com>
2017-05-09 07:23:37 -0400

    Added more samples

--- importsamples.sh
+++ importsamples.sh
@@ -6,3 +6,9 @@ $SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_2_1" 
 $SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_2_2" -f $SAMPLE_PIPELINES/Lesson_2_2.json
 $SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_1" -f $SAMPLE_PIPELINES/Lesson_3_1.json
 $SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_2" -f $SAMPLE_PIPELINES/Lesson_3_2.json
+$SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_3" -f $SAMPLE_PIPELINES/Lesson_3_3.json
+$SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_4" -f $SAMPLE_PIPELINES/Lesson_3_4.json
+$SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_5" -f $SAMPLE_PIPELINES/Lesson_3_5.json
+$SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_6" -f $SAMPLE_PIPELINES/Lesson_3_6.json
+$SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_7_Producer" -f $SAMPLE_PIPELINES/Lesson_3_7_Producer.json
+$SDC/streamsets cli -U http://localhost:18630 store import -n "DEMO_Lesson_3_7_Consumer" -f $SAMPLE_PIPELINES/Lesson_3_7_Consumer.json
